04bd241f4cd29727d411ba227aa0399e6172a4c5
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-03-13 10:48:12 +0100

    my javascript code

--- javascript/functions.js
+++ javascript/functions.js
@@ -236,7 +236,7 @@ let number = 7;
 
 for(let i = 1; i <= number; i++){
     for(let j = 1; j <= i; j++){
-       process.stdout.write("#");
+    process.stdout.write("#");
     }
     console.log();
 }
