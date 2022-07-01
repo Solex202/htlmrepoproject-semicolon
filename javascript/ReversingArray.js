d59ca88cc08eda458d540c2965d8fa36ca266e25
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-07-01 15:54:17 +0100

    update

---
+++ javascript/ReversingArray.js
@@ -0,0 +1,22 @@
+// let anArray = [2,3,4,57,8,9].reverse()
+let anArray = [2,3,4,57,8,9]
+
+function reverseArray(array){
+    for(let i = array.length; i >= 0; i--){
+        console.log(array[i]);
+    }
+}
+
+reverseArray(anArray);
+// console.log(anArray);
+
+
+function greaterThan(n) {
+    return m => m > n;
+    for(let element of array){
+        
+    }
+    }
+    let greaterThan10 = greaterThan(10);
+    console.log(greaterThan10(10));
+    
